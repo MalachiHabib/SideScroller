37d771fc7527b768fcc656dd821e54ffe5ad2924
DESKTOP-IUIMVAD\Windows10 <Malachi.Habib@outlook.com>
2023-04-10 20:39:27 +0930

    init

---
+++ android/src/com/malachi/sidescrollergame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.malachi.sidescrollergame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.malachi.sidescrollergame.SideScrollerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SideScrollerGame(), config);
+	}
+}
